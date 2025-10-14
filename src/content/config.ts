1ccfad49e289423d3d7bbbf15610a5694d40fd06
TheLogan21 <miguel_dmartillo@outlook.com>
2025-10-13 23:44:13 -0500

    BLOG-OK

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,18 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 
 
 const blogCollection = defineCollection({
     type: "content",
-    schema: ({image})=> z.object({
+    schema: ({ image }) => z.object({
         title: z.string(),
         date: z.date(),
         description: z.string(),
         image: image(),
 
         // relacion
-        author: z.string(),
+        // author: z.string(),
+        author: reference('author'),
         // relacion
         tags: z.array(z.string()),
 
@@ -19,8 +20,16 @@ const blogCollection = defineCollection({
     })
 })
 
+const authorCollection = defineCollection({
+    type: "data",
+    schema: ({ image }) => z.object({
+        name: z.string(),
+        avatar: image(),
 
+    }),
+})
 
 export const collections = {
     blog: blogCollection,
+    author: authorCollection
 }
